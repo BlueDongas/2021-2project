c8d39b86a247b088bd5fa6ce68de561db2db2ce3
javascriptKR <sssm0928@naver.com>
2021-09-24 17:38:43 +0900

    modify many files 1.1.3

--- extension/js/shortcut.js
+++ extension/js/shortcut.js
@@ -1,29 +1,65 @@
 // let img = document.getElementById('img');
 
-document.onclick = shortcut;
+// document.onclick = shortcut;
 
 // document.getElementById('img').onclick = function() {
-function shortcut(){
-// img.onclick = function() {
-    const WebName = prompt("이름","바로가기");
-    const URLADDR = prompt("URL","주소");
+// function shortcut(){
+//     const WebName = prompt("이름","바로가기");
+//     const URLADDR = prompt("URL","주소");
+    
+//     localStorage.setItem('name', WebName)
+//     localStorage.setItem('url', URLADDR)
+//     createDiv(WebName, URLADDR);
+// }
+
+// function createDiv(WebName, URLADDR) {
+//     const newDiv = document.createElement('div');
+    
+//     const newText = document.createTextNode(WebName);
+
+//     newDiv.appendChild(newText);
+//     // newDiv.onclick = location.href="https://"+URLADDR;
+    
+//     document.body.appendChild(newDiv);
+// } 
+
+// function deleteDiv() {
+//     const div = document.getElementById('my_div');
+
+//     div.remove();
+// }
+
+let WebName;
+let URLADDR;
+
+document.addEventListener('DOMContentLoaded', function() {
+    var hehe = document.getElementById('plus');
+
+    function addDiv(){
+        const newName = document.createElement('div');
+        const newURL = document.createElement('div');
+
+        const shortcut = document.createElement('div');
         
-    createDiv(WebName, URLADDR);
-}
+        shortcut.className = "shortcut";
+        
+        newName.innerHTML = WebName;
+        newURL.innerHTML = URLADDR;
 
-function createDiv(WebName, URLADDR) {
-    const newDiv = document.createElement('div');
+        shortcut.appendChild(newName);
+        shortcut.appendChild(newURL);
+
+        document.getElementById('short').appendChild(shortcut);
+    }
+
+    hehe.addEventListener('click', function() {
+        WebName = prompt("이름","바로가기");
+        URLADDR = prompt("URL","주소");
+
+        localStorage.setItem('name', WebName);
+        localStorage.setItem('url', URLADDR);
+
+        addDiv();
+    });
     
-    const newText = document.createTextNode(WebName);
-
-    newDiv.appendChild(newText);
-    newDiv.onclick = location.href="https://"+URLADDR;
-    
-    document.body.appendChild(newDiv);
-} 
-
-function deleteDiv() {
-    const div = document.getElementById('my_div');
-
-    div.remove();
-}
+});
