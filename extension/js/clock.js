c039e8c61a6c0bf8adc3bd26dbf68ced7c47675a
BlueDongas <web3570@gmail.com>
2021-09-27 05:24:45 +0900

    add select day,hour,minute function

--- extension/js/clock.js
+++ extension/js/clock.js
@@ -4,29 +4,46 @@ document.addEventListener("DOMContentLoaded",function(){
     if((window.location.href).includes('setting.html')){  
         var setbtn = document.getElementById('setting_dday');
         var namebtn = document.getElementById('setting-dday-name');
+        var timeSel = document.getElementById('timesel');
+  
         setbtn.addEventListener('click',function(){
             var dday = document.querySelector('#dday').value.replace(/-/g,'.'); // 형식 변환해서 00시로 초기화하기 위함
             var name = document.querySelector('#dday-name').value;
+            var timeOption = timeSel.options[timeSel.selectedIndex].text;
 
+            localStorage.setItem('timeoption',timeOption);
             localStorage.setItem('Dday-name',name);
             localStorage.setItem('Dday',dday);
-            location.reload();
+            location.includes('index.html').reload();
         });
     }
     else{
         function DisplayClock(){
             const dayClock = document.getElementById('day_clock');
-
+            
             var name = localStorage.getItem('Dday-name');
             var dday = new Date(localStorage.getItem('Dday'));
             var gap = dday-today;
             var day = Math.floor(gap/(1000*60*60*24));
-            var hour = Math.floor(gap%(1000*60*60*24)/(1000*60*60));
+            var hour = Math.floor(gap%(1000*60*60*24)/(1000*60*60))+day*24;
+            var minute = Math.floor((gap%(1000*60*60))/(1000*60))+hour*60;
 
-            dayClock.innerHTML = name+"까지 "+ day + "일 " + hour + "시간";
+            if(localStorage.getItem('timeoption') == "Day"){
+                dayClock.innerHTML = name+"까지 "+ day + "일 ";
+            }
+            else if(localStorage.getItem('timeoption') == "Hour"){
+                dayClock.innerHTML = name+"까지 "+ hour + "시간";
+            }
+            else if(localStorage.getItem('timeoption') == "Minute"){
+                dayClock.innerHTML = name+"까지 "+ minute + "분";
+            }
         }
 
-        DisplayClock();
+        function init(){
+            DisplayClock();
+            setInterval(DisplayClock,1000*60);
+        }
 
+        init();
     }
 });
